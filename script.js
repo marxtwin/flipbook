cfce957998ee504f916a4d7a1d08ee620205ae4a
marxtwin <120964691+marxtwin@users.noreply.github.com>
2024-07-19 18:44:25 +0200

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let currentPage = 0;
-const pages = document.querySelectorAll('.page');
+document.addEventListener("DOMContentLoaded", function () {
+    let currentPage = 0;
+    const pages = document.querySelectorAll('#flipbook .page');
 
-function showPage(index) {
-    const angle = index * -180;
-    document.querySelector('.flipbook').style.transform = `rotateY(${angle}deg)`;
-}
+    function showPage(index) {
+        const angle = index * -180;
+        document.querySelector('#flipbook').style.transform = `rotateY(${angle}deg)`;
+    }
 
-pages.forEach((page, index) => {
-    page.addEventListener('click', () => {
-        if (index % 2 === 0 && index < pages.length - 1) {
-            currentPage++;
-        } else if (index % 2 !== 0 && index > 0) {
-            currentPage--;
-        }
-        showPage(currentPage);
+    pages.forEach((page, index) => {
+        page.addEventListener('click', () => {
+            if (index % 2 === 0 && index < pages.length - 1) {
+                currentPage++;
+            } else if (index % 2 !== 0 && index > 0) {
+                currentPage--;
+            }
+            showPage(currentPage);
+        });
     });
 });
