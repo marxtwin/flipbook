6b9a4dff76aa32debec846e379d53343b0e86905
marxtwin <120964691+marxtwin@users.noreply.github.com>
2024-07-19 18:51:13 +0200

    script.js

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const pages = document.querySelectorAll('#flipbook .page');
 
     function showPage(index) {
-        pages.forEach((page, i) => {
-            page.style.display = (i === index || i === index - 1) ? 'flex' : 'none';
-        });
         const angle = index * -180;
         document.querySelector('#flipbook').style.transform = `rotateY(${angle}deg)`;
     }
@@ -20,6 +17,4 @@ document.addEventListener("DOMContentLoaded", function () {
             showPage(currentPage);
         });
     });
-
-    showPage(currentPage);  // Show the first page
 });
